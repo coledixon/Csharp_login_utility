b0dbd2c455ad35091c399ae94a27feadda79b19f
colefromportland <colefromportland@gmail.com>
2019-04-15 13:20:09 -0600

    designing GUI layout
    building custom base GUI design and layout

--- LoginApp_app/LoginApp/Login_App.Designer.cs
+++ LoginApp_app/LoginApp/Login_App.Designer.cs
@@ -30,53 +30,38 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login_App));
             this.grpCreds = new System.Windows.Forms.GroupBox();
-            this.userIcon = new System.Windows.Forms.PictureBox();
-            this.passIcon = new System.Windows.Forms.PictureBox();
             this.txtUsername = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
-            this.btnLogin = new System.Windows.Forms.Button();
+            this.grpStatus = new System.Windows.Forms.GroupBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnAdmin = new System.Windows.Forms.Button();
             this.btnLogout = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.passIcon = new System.Windows.Forms.PictureBox();
+            this.userIcon = new System.Windows.Forms.PictureBox();
             this.grpCreds.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.userIcon)).BeginInit();
+            this.grpStatus.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.passIcon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userIcon)).BeginInit();
             this.SuspendLayout();
             // 
             // grpCreds
             // 
+            this.grpCreds.BackColor = System.Drawing.SystemColors.Control;
             this.grpCreds.Controls.Add(this.txtPassword);
             this.grpCreds.Controls.Add(this.txtUsername);
             this.grpCreds.Controls.Add(this.passIcon);
             this.grpCreds.Controls.Add(this.userIcon);
             this.grpCreds.Location = new System.Drawing.Point(13, 13);
             this.grpCreds.Name = "grpCreds";
-            this.grpCreds.Size = new System.Drawing.Size(370, 169);
+            this.grpCreds.Size = new System.Drawing.Size(381, 169);
             this.grpCreds.TabIndex = 0;
             this.grpCreds.TabStop = false;
             this.grpCreds.Text = "Login Credentials";
             // 
-            // userIcon
-            // 
-            this.userIcon.Image = global::LoginApp.Properties.Resources.user;
-            this.userIcon.InitialImage = global::LoginApp.Properties.Resources.user;
-            this.userIcon.Location = new System.Drawing.Point(6, 36);
-            this.userIcon.Name = "userIcon";
-            this.userIcon.Size = new System.Drawing.Size(41, 41);
-            this.userIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.userIcon.TabIndex = 0;
-            this.userIcon.TabStop = false;
-            // 
-            // passIcon
-            // 
-            this.passIcon.Image = global::LoginApp.Properties.Resources.password;
-            this.passIcon.Location = new System.Drawing.Point(7, 98);
-            this.passIcon.Name = "passIcon";
-            this.passIcon.Size = new System.Drawing.Size(41, 41);
-            this.passIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.passIcon.TabIndex = 1;
-            this.passIcon.TabStop = false;
-            // 
             // txtUsername
             // 
+            this.txtUsername.BackColor = System.Drawing.SystemColors.Window;
             this.txtUsername.Location = new System.Drawing.Point(68, 48);
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Size = new System.Drawing.Size(282, 22);
@@ -89,6 +74,51 @@
             this.txtPassword.Size = new System.Drawing.Size(282, 22);
             this.txtPassword.TabIndex = 3;
             // 
+            // grpStatus
+            // 
+            this.grpStatus.BackColor = System.Drawing.Color.AliceBlue;
+            this.grpStatus.Controls.Add(this.lblStatus);
+            this.grpStatus.Location = new System.Drawing.Point(100, 189);
+            this.grpStatus.Name = "grpStatus";
+            this.grpStatus.Size = new System.Drawing.Size(139, 44);
+            this.grpStatus.TabIndex = 3;
+            this.grpStatus.TabStop = false;
+            this.grpStatus.Text = "STATUS";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("MS Gothic", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(46, 18);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(87, 14);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "LOGGED OUT";
+            // 
+            // btnAdmin
+            // 
+            this.btnAdmin.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnAdmin.BackgroundImage = global::LoginApp.Properties.Resources.admin;
+            this.btnAdmin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnAdmin.Location = new System.Drawing.Point(328, 189);
+            this.btnAdmin.Name = "btnAdmin";
+            this.btnAdmin.Size = new System.Drawing.Size(58, 45);
+            this.btnAdmin.TabIndex = 4;
+            this.btnAdmin.UseVisualStyleBackColor = false;
+            this.btnAdmin.Visible = false;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnLogout.BackgroundImage = global::LoginApp.Properties.Resources.logout;
+            this.btnLogout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnLogout.Location = new System.Drawing.Point(264, 188);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(58, 45);
+            this.btnLogout.TabIndex = 2;
+            this.btnLogout.UseVisualStyleBackColor = false;
+            this.btnLogout.Visible = false;
+            // 
             // btnLogin
             // 
             this.btnLogin.BackColor = System.Drawing.SystemColors.ActiveBorder;
@@ -100,33 +130,49 @@
             this.btnLogin.TabIndex = 1;
             this.btnLogin.UseVisualStyleBackColor = false;
             // 
-            // btnLogout
+            // passIcon
             // 
-            this.btnLogout.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.btnLogout.BackgroundImage = global::LoginApp.Properties.Resources.logout;
-            this.btnLogout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnLogout.Location = new System.Drawing.Point(97, 188);
-            this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(58, 45);
-            this.btnLogout.TabIndex = 2;
-            this.btnLogout.UseVisualStyleBackColor = false;
-            this.btnLogout.Visible = false;
+            this.passIcon.Image = global::LoginApp.Properties.Resources.password;
+            this.passIcon.Location = new System.Drawing.Point(7, 98);
+            this.passIcon.Name = "passIcon";
+            this.passIcon.Size = new System.Drawing.Size(41, 41);
+            this.passIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.passIcon.TabIndex = 1;
+            this.passIcon.TabStop = false;
+            // 
+            // userIcon
+            // 
+            this.userIcon.Image = global::LoginApp.Properties.Resources.user;
+            this.userIcon.InitialImage = global::LoginApp.Properties.Resources.user;
+            this.userIcon.Location = new System.Drawing.Point(6, 36);
+            this.userIcon.Name = "userIcon";
+            this.userIcon.Size = new System.Drawing.Size(41, 41);
+            this.userIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.userIcon.TabIndex = 0;
+            this.userIcon.TabStop = false;
             // 
             // Login_App
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(408, 253);
+            this.Controls.Add(this.btnAdmin);
+            this.Controls.Add(this.grpStatus);
             this.Controls.Add(this.btnLogout);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.grpCreds);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(426, 300);
+            this.MinimumSize = new System.Drawing.Size(426, 300);
             this.Name = "Login_App";
             this.Text = "Login Utility";
             this.grpCreds.ResumeLayout(false);
             this.grpCreds.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.userIcon)).EndInit();
+            this.grpStatus.ResumeLayout(false);
+            this.grpStatus.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.passIcon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userIcon)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -140,6 +186,9 @@
         private System.Windows.Forms.PictureBox passIcon;
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.GroupBox grpStatus;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnAdmin;
     }
 }
 
--- LoginApp_app/LoginApp/Properties/Resources.Designer.cs
+++ LoginApp_app/LoginApp/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace LoginApp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap admin {
+            get {
+                object obj = ResourceManager.GetObject("admin", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap change_pass {
             get {
                 object obj = ResourceManager.GetObject("change_pass", resourceCulture);
