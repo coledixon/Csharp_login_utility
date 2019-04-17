19b363d44b7a0f038b0c6c232ddef6dbc0bc57c7
colefromportland <colefromportland@gmail.com>
2019-04-17 10:39:21 -0600

    building base config for app / VS env

---
+++ LoginApp_app/LoginApp/Controllers/loginData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginApp.Controllers
+{
+    class loginData
+    {
+        // INSTANTIATE CLASS(ES)
+
+
+        // data conn
+        static string dataconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+    }
+}
---
+++ LoginApp_app/LoginApp/Models/loginProps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginApp.Models
+{
+    class loginProps
+    {
+        // SQL col names not same as props (schema cols include _)
+        #region get/set
+        // user_main
+        public int UserKey { get; set; }
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        // pass_main
+        public string PassHash { get; set; }
+        public string PassSalt { get; set; }
+
+        // login_audit
+        public int LoginStatus { get; set; }
+        public string CurrSessId { get; set; }
+        public DateTime LastLogin { get; set; }
+
+        // logout_audit
+        public int LogoutStatus { get; set; }
+        public DateTime LastLogout { get; set; }
+        public string LastSessId { get; set; }
+
+        // user_key_store
+        public int NewUserKey { get; set; }
+        #endregion
+    }
+}
