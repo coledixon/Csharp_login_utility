4b090f66d5e7e12f790e13ebfb7614fd94279567
colefromportland <colefromportland@gmail.com>
2019-09-25 11:59:33 -0600

    base functionality for C# pw hashing

--- LoginApp_app/LoginApp/Controllers/loginHash.cs
+++ LoginApp_app/LoginApp/Controllers/loginHash.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,19 @@ namespace LoginApp.Controllers
 {
     class loginHash
     {
-        // main hashing class (hash pw in C# prior to SQL salt) 
+        // CD: testing base functionality for parsing hash
+        public static string hashSHA2_512(string val)
+        {
+            var sha2_512 = System.Security.Cryptography.SHA512.Create();
+            var bytes = Encoding.ASCII.GetBytes(val);
+            var hash = sha2_512.ComputeHash(bytes);
+
+            var sb = new StringBuilder();
+            for (var i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
     }
 }
